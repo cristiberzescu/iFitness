2ef3a9ddcb0cfb05eff379e897740bcb27e884e2
cristiberzescu <cristiberzescu>
2023-01-16 13:08:07 +0200

    working on register and login

---
+++ app/src/main/java/com/example/ifitness/ProfileCharacteristics.kt
@@ -0,0 +1,60 @@
+package com.example.ifitness
+
+object ProfileCharacteristics {
+
+    private var username: String? = null
+    private var email: String? = null
+    private var key : String? = null
+    private var phonenumber : String? = null
+    private var location : String? = null
+    private var signaltransit : String? = null
+
+    fun getLocation(): String?{
+        return location
+    }
+
+    fun setLocation(savelocation : String){
+        location = savelocation
+    }
+
+    fun getSignaltransit(): String?{
+        return signaltransit
+    }
+
+    fun setSignaltransit(transit: String){
+        signaltransit = transit
+    }
+
+    fun getKey(): String?{
+        return key
+    }
+
+    fun setKey(savekey: String){
+        key = savekey
+    }
+
+    fun getUsername(): String? {
+        return username
+    }
+
+    fun setUsername(savedata: String) {
+        username = savedata
+    }
+
+    fun getEmail(): String?{
+        return email
+    }
+
+    fun setEmail(saveemail: String){
+        email = saveemail
+    }
+
+    fun getPhonenumber(): String?{
+        return phonenumber
+    }
+
+    fun setPhonenumber(savepnone : String){
+        phonenumber = savepnone
+    }
+
+}
---
+++ app/src/main/java/com/example/ifitness/User.kt
@@ -0,0 +1,8 @@
+package com.example.ifitness
+
+data class User(
+    //var userId: String? = null,
+    var userEmail: String? = null,
+    var userName: String? = null,
+    var userPassword: String? = null
+)
