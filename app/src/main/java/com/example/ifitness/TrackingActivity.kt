c41329d5f5f3dbf067b827d60a95ec68df586c15
cristiberzescu <cristiberzescu>
2023-01-19 03:57:36 +0200

    create activities

--- app/src/main/java/com/example/ifitness/TrackingActivity.kt
+++ app/src/main/java/com/example/ifitness/TrackingActivity.kt
@@ -4,25 +4,18 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageButton
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 
-class MainActivity : ComponentActivity() {
+class TrackingActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.main_activity)
+        setContentView(R.layout.tracking_activity)
 
-        var buttonBack = findViewById(R.id.btn_back) as Button
         var buttonMenu = findViewById(R.id.main_button) as ImageButton
         var buttonCalories = findViewById(R.id.calories_button) as ImageButton
         var buttonWorkouts = findViewById(R.id.workouts_button) as ImageButton
         var buttonTracking = findViewById(R.id.tracking_button) as ImageButton
         var buttonMap = findViewById(R.id.map_button) as ImageButton
-
-        buttonBack.setOnClickListener {
-            val intent = Intent(this, LoginActivity::class.java)
-            startActivity(intent)
-        }
 
         buttonMenu.setOnClickListener {
             val intent = Intent(this, MainActivity::class.java)
@@ -44,6 +37,7 @@ class MainActivity : ComponentActivity() {
             val intent = Intent(this, MapActivity::class.java)
             startActivity(intent)
         }
+
 
     }
 }
