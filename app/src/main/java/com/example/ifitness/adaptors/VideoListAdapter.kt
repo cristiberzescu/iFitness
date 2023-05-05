ba6f1b8f0e09feade84fe5791490ff8418f1ec84
cristiberzescu <berzescu.c31@gmail.com>
2023-05-05 17:09:41 +0300

    work on tracking activity

--- app/src/main/java/com/example/ifitness/adaptors/VideoListAdapter.kt
+++ app/src/main/java/com/example/ifitness/adaptors/VideoListAdapter.kt
@@ -7,39 +7,35 @@ import android.widget.TextView
 import android.widget.VideoView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.ifitness.R
-import com.example.ifitness.domain.Exercise
-import com.example.ifitness.domain.Series
 import com.example.ifitness.domain.VideoExercise
 
-class ExerciseListAdapter(private val exerciseList: ArrayList<Exercise>) :
-    RecyclerView.Adapter<ExerciseListAdapter.ExerciseViewHolder>() {
+class VideoListAdapter(private val exerciseList: ArrayList<VideoExercise>) :
+    RecyclerView.Adapter<VideoListAdapter.ExerciseViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ExerciseViewHolder {
         val itemView =
-            LayoutInflater.from(parent.context).inflate(R.layout.exercise_design, parent, false)
+            LayoutInflater.from(parent.context).inflate(R.layout.video_exercise_design, parent, false)
         return ExerciseViewHolder(itemView)
     }
 
     override fun onBindViewHolder(holder: ExerciseViewHolder, position: Int) {
         val exercise = exerciseList[position]
         holder.nameExercise.text = exercise.name
-        holder.setSeriesList(exercise.series as ArrayList<Series>)
+        holder.videoExercise.setVideoPath(exercise.url)
+        holder.videoExercise.start()
+        holder.videoExercise.setOnPreparedListener { mediaPlayer ->
+            mediaPlayer.isLooping = true
+        }
+
+
     }
-
-
 
     override fun getItemCount() = exerciseList.size
 
     class ExerciseViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val nameExercise: TextView = itemView.findViewById(R.id.exercise_name)
-        val seriesRecyclerView: RecyclerView = itemView.findViewById(R.id.foodList)
-        val seriesAdapter = SeriesListAdapter(ArrayList<Series>())
-
-        fun setSeriesList(seriesList: ArrayList<Series>) {
-            seriesAdapter.setSeriesList(seriesList)
-            seriesRecyclerView.adapter?.notifyDataSetChanged()
-        }
+        val videoExercise: VideoView = itemView.findViewById(R.id.video_exercise)
     }
-
-
 }
+
+
