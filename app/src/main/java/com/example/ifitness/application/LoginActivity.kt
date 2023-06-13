ffff8ef9bbfd9cbecbef36c1e45bdeab08ce1f8a
cristiberzescu <81633800+cristiberzescu@users.noreply.github.com>
2023-06-13 18:36:57 +0300

    Update LoginActivity.kt

--- app/src/main/java/com/example/ifitness/application/LoginActivity.kt
+++ app/src/main/java/com/example/ifitness/application/LoginActivity.kt
@@ -20,7 +20,6 @@ import java.security.MessageDigest
 class LoginActivity : ComponentActivity() {
     private var firebaseDatabase: FirebaseDatabase? = null
     private var databaseReference: DatabaseReference? = null
-
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,29 +28,24 @@ class LoginActivity : ComponentActivity() {
             WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN
         );
         setContentView(R.layout.login_activity)
-
         firebaseDatabase = FirebaseDatabase.getInstance()
         databaseReference = firebaseDatabase?.getReference("users")
-
         var count: Int? = 0
         var toast: Toast = Toast.makeText(
             applicationContext,
             "User name or Password incorrect!",
             Toast.LENGTH_SHORT
         )
-
         var name = findViewById(R.id.et_user_name) as EditText
         var password = findViewById(R.id.et_password) as EditText
         var buttonLogin = findViewById(R.id.btn_login) as Button
         var buttonRegister = findViewById(R.id.btn_register) as Button
         buttonLogin.setOnClickListener {
-
             fun encryptPassword(password: String): String {
                 val digest = MessageDigest.getInstance("SHA-256")
                 val bytes = digest.digest(password.toByteArray())
                 return bytes.joinToString("") { "%02x".format(it) }
             }
-
             val intent = Intent(this, MainActivity::class.java)
             databaseReference?.addListenerForSingleValueEvent(object : ValueEventListener {
                 override fun onDataChange(snapshot: DataSnapshot) {
@@ -62,7 +56,6 @@ class LoginActivity : ComponentActivity() {
                         val userEmail = ds.child("userEmail").value.toString()
 
                         if (name.text.toString() == userName && encryptPassword(password.text.toString()) == userPassword) {
-
                             UserCharacteristics.setUsername(userName)
                             UserCharacteristics.setEmail(userEmail)
                             startActivity(intent)
@@ -73,24 +66,19 @@ class LoginActivity : ComponentActivity() {
                         }
                     }
                 }
-
                 override fun onCancelled(error: DatabaseError) {
                     Log.e("ooooo", "onCancelled: ${error.toException()}")
                 }
             })
-
             Handler().postDelayed({
                 if (count == 0) {
                     toast.show()
                 }
             }, 150)
-
         }
-
         buttonRegister.setOnClickListener {
             val intent = Intent(this, RegisterActivity::class.java)
             startActivity(intent)
         }
-
     }
 }
